135e62053057a6aa81b4d3338e00eb5362879b4b
SuperNever <1021165525@qq.com>
2017-10-27 10:57:30 +0800

    initial

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>test</h1>
+    <p>A vue project.</p>
+    <button type="button" @click="btnClick" value="点击我">sdsdsd</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    methods: {
+      btnClick() {
+        alert('click');
+      }
+    }
+  };
+</script>
